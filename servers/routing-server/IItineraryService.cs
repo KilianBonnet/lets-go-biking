3862177c7cd104787ac9045888d342e29deecbda
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-11-16 15:36:02 +0100

    Adding routing server

---
+++ servers/routing-server/IItineraryService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace routing_server
+{
+    [ServiceContract]
+    public interface IItineraryService
+    {
+        [OperationContract]
+        string GetData(int value);
+    }
+}
---
+++ servers/routing-server/ItineraryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace routing_server
+{
+    public class ItineraryService : IItineraryService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
